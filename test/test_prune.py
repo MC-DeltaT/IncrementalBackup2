594fc144f4c0a0d4d82298f8ee698bb639d31dc7
Reece Jones <reece.jones131@gmail.com>
2024-03-28 21:36:56 +1100

    Testing prune_backups

--- test/test_prune.py
+++ test/test_prune.py
@@ -1,8 +1,13 @@
 from pathlib import Path
+from typing import Any
 
-from incremental_backup.prune import BackupPrunabilityOptions, is_backup_prunable
+import pytest
 
-from helpers import write_valid_backup
+from incremental_backup.meta.meta import ReadBackupsCallbacks
+from incremental_backup.prune import BackupPrunabilityOptions, is_backup_prunable, prune_backups, PruneBackupsCallbacks, \
+    PruneBackupsConfig, PruneBackupsError
+
+from helpers import AssertFilesystemUnmodified, write_valid_backup
 
 
 def test_is_backup_prunable_nonempty(tmpdir: Path) -> None:
@@ -45,4 +50,45 @@ def test_is_backup_prunable_other_data(tmpdir: Path) -> None:
     assert is_backup_prunable(tmpdir, backup, BackupPrunabilityOptions(prune_empty=True, prune_other_data=True))
 
 
-# TODO: test prune_backups
+def test_prune_backups_nonexistent_target(tmpdir: Path) -> None:
+    # Backup target directory doesn't exist.
+
+    target_dir = tmpdir / 'backups'
+
+    config = PruneBackupsConfig(False, BackupPrunabilityOptions(True, False))
+
+    actual_callbacks: list[Any] = []
+
+    callbacks = PruneBackupsCallbacks(
+        on_before_read_backups=lambda: actual_callbacks.append('on_before_read_backups'),
+        read_backups=ReadBackupsCallbacks(
+            on_query_entry_error=lambda path, error: pytest.fail(f'Unexpected on_query_entry_error: {path=} {error=}'),
+            on_read_metadata_error=lambda path, error:
+                pytest.fail(f'Unexpected on_read_metadata_error: {path=} {error=}')
+        ),
+        on_after_read_backups=lambda backups: pytest.fail(f'Unexpected on_after_read_backups: {backups=}'),
+        on_selected_backups=lambda backups: pytest.fail(f'Unexpected on_selected_backups: {backups=}'),
+        on_delete_error=lambda path, error: pytest.fail(f'Unexpected on_delete_error: {path=} {error=}'),
+    )
+
+    with AssertFilesystemUnmodified(tmpdir):
+        with pytest.raises(PruneBackupsError):
+            prune_backups(target_dir, config, callbacks)
+    
+    assert actual_callbacks == [
+        'on_before_read_backups'
+    ]
+
+
+def test_prune_backups_invalid_backup(tmpdir: Path) -> None:
+    # Some backups have invalid metadata.
+
+    # TODO
+    pytest.fail()
+
+
+def test_prune_backups_delete_empty(tmpdir: Path) -> None:
+    # Normal behaviour of deleting empty backups.
+
+    # TODO
+    pytest.fail()
